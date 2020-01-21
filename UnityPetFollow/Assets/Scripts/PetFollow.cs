29a170cc228c8387cf3d0b62a5ccb3798306e319
ben33779610 <49440361+ben33779610@users.noreply.github.com>
2020-01-21 17:22:48 +0800

    跟隨

---
+++ UnityPetFollow/Assets/Scripts/PetFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetFollow : MonoBehaviour
+{
+
+    private Transform player;
+    public float speed = 1.5f;
+    public Transform target;
+
+    private void Start()
+    {
+        player = GameObject.Find("野蠻人").GetComponent<Transform>();
+    }
+
+    /*private void LateUpdate()
+    {
+        Follow();
+    }*/
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    private void Follow() 
+    {
+        Vector3 posP = player.position;
+        Vector3 posC = transform.position;
+
+        posP.z += -5;
+
+        transform.position = Vector3.Lerp(posC, posP, 0.3f * Time.deltaTime * speed);
+        transform.LookAt(target);
+    }
+
+}
